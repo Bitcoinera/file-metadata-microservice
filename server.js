cd113e8096eccf01160c187c4b8d26623c536acc
anagutjor <anagjordano@gmail.com>
2019-01-28 15:35:10 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer();
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  let file = req.file;
+
+  if (file === undefined) {
+    res.send('File not recognized');
+  } else {
+  res.json({filename: file.originalname, type: file.mimetype, size: file.size});
+  }
+  next();
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
